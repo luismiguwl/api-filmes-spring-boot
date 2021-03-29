56bb9411c6e58b57b2a89611fd97afa007e530ce
Luis Miguel <luisbwrb@gmail.com>
2021-03-28 23:08:57 -0300

    conclusao do switch contendo o nome do mes para cada numero

--- src/main/java/br/com/luis/api/models/utils/MesUtils.java
+++ src/main/java/br/com/luis/api/models/utils/MesUtils.java
@@ -21,6 +21,22 @@ public class MesUtils {
 		    	return "Março";
 		    case 4:
 		    	return "Abril";
+		    case 5:
+		    	return "Maio";
+		    case 6:
+		    	return "Junho";
+			case 7:
+				return "Julho";
+			case 8:
+				return "Agosto";
+			case 9:
+				return "Setembro";
+			case 10:
+				return "Outubro";
+			case 11:
+				return "Novembro";
+			case 12:
+				return "Dezembro";
 		}
 		
 		return null;
