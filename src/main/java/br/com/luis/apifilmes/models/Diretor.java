75438bbe509da09a83401a9af1388fd4d585443d
Luis Miguel <luisbwrb@gmail.com>
2021-04-25 19:01:56 -0300

    outra melhoria na classe Diretor

--- src/main/java/br/com/luis/apifilmes/models/Diretor.java
+++ src/main/java/br/com/luis/apifilmes/models/Diretor.java
@@ -4,21 +4,17 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import br.com.luis.apifilmes.models.utils.DiretorUtils;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class Diretor {
 	private String nome;
-	private Integer quantidadeDeFilmesVistos;
 
-	public Diretor(String nome) {
-		this.nome = nome;
-	}
-	
 	public Integer getQuantidadeDeFilmesVistos() {
-		quantidadeDeFilmesVistos = DiretorUtils.getQuantidadeDeFilmesVistos(this);
+		int quantidadeDeFilmesVistos = DiretorUtils.getQuantidadeDeFilmesVistos(this);
 		return quantidadeDeFilmesVistos > 0 ? quantidadeDeFilmesVistos : null;
 	}
-
 }
