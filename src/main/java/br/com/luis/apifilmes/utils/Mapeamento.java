e2c86974c1b9c7f50d28f3525ffa39e9a52cc66e
luismiguwl <luisbwrb@gmail.com>
2021-07-14 00:42:15 -0300

    Migrando para o Apache Commons CSV

--- src/main/java/br/com/luis/apifilmes/utils/Mapeamento.java
+++ src/main/java/br/com/luis/apifilmes/utils/Mapeamento.java
@@ -3,15 +3,17 @@ package br.com.luis.apifilmes.utils;
 import static br.com.luis.apifilmes.models.utils.MapeamentoUtils.*;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.csvreader.CsvReader;
 
 import br.com.luis.apifilmes.models.*;
 
@@ -23,33 +25,44 @@ public class Mapeamento {
         String destino = tipo.getDestino();
 
         try {
-            CsvReader csv = new CsvReader(new InputStreamReader(new FileInputStream(destino), "UTF-8"));
-            csv.readHeaders();
+            Reader in = new FileReader(destino);
 
-            while (csv.readRecord()) {
-                List<Diretor> diretores = new ArrayList<>();
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT
+            		.withFirstRecordAsHeader()
+            		.parse(in);
+            
+            for (CSVRecord record : records) {
+            	String data = null;
+            	
+            	List<Diretor> diretores = new ArrayList<>();
                 Diretor diretor = null;
                 
                 List<Genero> generos = new ArrayList<>();
                 Genero genero = null;
 
-                String titulo = csv.get(Coluna.TITULO.getColuna());
-                String data = csv.get(Coluna.DATA_ASSISTIDO.getColuna());
-                int ano = Integer.parseInt(csv.get(Coluna.ANO_LANCAMENTO.getColuna()));
-                Idioma idioma = new Idioma(csv.get(Coluna.IDIOMA.getColuna()));
+                String titulo = record.get(Coluna.TITULO.getColuna());
+                
+                System.out.println(titulo);
+                
+                if (tipo.equals(TipoDeConsulta.VISTOS)) {
+                	data = record.get(Coluna.DATA_ASSISTIDO.getColuna());
+                }
+                
+                int ano = Integer.parseInt(record.get(Coluna.ANO_LANCAMENTO.getColuna()));
+                Idioma idioma = new Idioma(record.get(Coluna.IDIOMA.getColuna()));
 
-                if (csv.get(Coluna.DIRETOR.getColuna()).contains(",")) {
-                    diretores = mapearDiretores(csv.get(Coluna.DIRETOR.getColuna()));
+                if (record.get(Coluna.DIRETOR.getColuna()).contains(",")) {
+                    diretores = mapearDiretores(record.get(Coluna.DIRETOR.getColuna()));
                 } else {
-                    diretor = new Diretor(csv.get(Coluna.DIRETOR.getColuna()));
+                    diretor = new Diretor(record.get(Coluna.DIRETOR.getColuna()));
                 }
 
-                int runtime = Integer.parseInt(csv.get(Coluna.DURACAO.getColuna()));
+                int runtime = Integer.parseInt(record.get(Coluna.DURACAO.getColuna()).trim());
                 
-                if (csv.get(Coluna.GENERO.getColuna()).contains(",")) {
-					generos = mapearGeneros(csv.get(Coluna.GENERO.getColuna()));
+                if (record.get(Coluna.GENERO.getColuna()).contains(",")) {
+					generos = mapearGeneros(record.get(Coluna.GENERO.getColuna()));
 				} else {
-					genero = new Genero(csv.get(Coluna.GENERO.getColuna()));
+					genero = new Genero(record.get(Coluna.GENERO.getColuna()));
 				}
 
                 Filme filme = new Filme(titulo, ano, data, idioma, diretor, diretores, genero, generos, runtime);
@@ -67,11 +80,14 @@ public class Mapeamento {
         String destino = TipoDeConsulta.ABREVIACOES.getDestino();
 
         try {
-            CsvReader csv = new CsvReader(new InputStreamReader(new FileInputStream(destino), "UTF-8"));
-            csv.readHeaders();
+        	Reader in = new FileReader(destino);
 
-            while (csv.readRecord()) {
-                abreviacoes.add(csv.get("idioma") + "," + csv.get("abreviacao"));
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT
+            		.withFirstRecordAsHeader()
+            		.parse(in);
+
+            for (CSVRecord record : records) {
+            	abreviacoes.add(record.get("idioma") + "," + record.get("abreviacao"));
             }
         } catch (IOException e) {
             logger.error("Arquivo não encontrado!");
@@ -86,11 +102,14 @@ public class Mapeamento {
         String nomeDaColuna = coluna.getColuna();
 
         try {
-            CsvReader csv = new CsvReader(new InputStreamReader(new FileInputStream(destino), "UTF-8"));
-            csv.readHeaders();
+        	Reader in = new FileReader(destino);
 
-            while (csv.readRecord()) {
-                dadosDaColuna.add(csv.get(nomeDaColuna));
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT
+            		.withFirstRecordAsHeader()
+            		.parse(in);
+
+            for (CSVRecord record : records) {
+            	dadosDaColuna.add(record.get(nomeDaColuna));
             }
         } catch (IOException e) {
             logger.error("Arquivo não encontrado!");
