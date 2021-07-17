219d009647df20497d383f705106c231c65f7bc2
luismiguwl <luisbwrb@gmail.com>
2021-07-17 04:52:34 -0300

    Removendo código boilerplate da classe Mapeamento

--- src/main/java/br/com/luis/apifilmes/utils/Mapeamento.java
+++ src/main/java/br/com/luis/apifilmes/utils/Mapeamento.java
@@ -2,118 +2,97 @@ package br.com.luis.apifilmes.utils;
 
 import static br.com.luis.apifilmes.models.utils.MapeamentoUtils.*;
 
-import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import br.com.luis.apifilmes.models.*;
 
 public class Mapeamento {
-    private static Logger logger = LoggerFactory.getLogger(Mapeamento.class);
+	public static List<Filme> getFilmes(TipoDeConsulta tipo) {
+		List<Filme> filmes = new ArrayList<>();
+		String destino = tipo.getDestino();
 
-    public static List<Filme> getFilmes(TipoDeConsulta tipo) {
-        List<Filme> filmes = new ArrayList<>();
-        String destino = tipo.getDestino();
+		Iterable<CSVRecord> records = lerArquivoCsv(destino);
 
-        try {
-            Reader in = new FileReader(destino);
+		for (CSVRecord record : records) {
+			String data = null;
 
-            Iterable<CSVRecord> records = CSVFormat.DEFAULT
-            		.withFirstRecordAsHeader()
-            		.parse(in);
-            
-            for (CSVRecord record : records) {
-            	String data = null;
-            	
-            	List<Diretor> diretores = new ArrayList<>();
-                Diretor diretor = null;
-                
-                List<Genero> generos = new ArrayList<>();
-                Genero genero = null;
+			List<Diretor> diretores = new ArrayList<>();
+			Diretor diretor = null;
 
-                String titulo = record.get(Coluna.TITULO.getColuna());
-                
-                if (tipo.equals(TipoDeConsulta.VISTOS)) {
-                	data = record.get(Coluna.DATA_ASSISTIDO.getColuna());
-                }
-                
-                int ano = Integer.parseInt(record.get(Coluna.ANO_LANCAMENTO.getColuna()));
-                Idioma idioma = new Idioma(record.get(Coluna.IDIOMA.getColuna()));
+			List<Genero> generos = new ArrayList<>();
+			Genero genero = null;
 
-                if (record.get(Coluna.DIRETOR.getColuna()).contains(",")) {
-                    diretores = mapearDiretores(record.get(Coluna.DIRETOR.getColuna()));
-                } else {
-                    diretor = new Diretor(record.get(Coluna.DIRETOR.getColuna()));
-                }
+			String titulo = record.get(Coluna.TITULO.getColuna());
 
-                int runtime = Integer.parseInt(record.get(Coluna.DURACAO.getColuna()).trim());
-                
-                if (record.get(Coluna.GENERO.getColuna()).contains(",")) {
-					generos = mapearGeneros(record.get(Coluna.GENERO.getColuna()));
-				} else {
-					genero = new Genero(record.get(Coluna.GENERO.getColuna()));
-				}
+			if (tipo.equals(TipoDeConsulta.VISTOS)) {
+				data = record.get(Coluna.DATA_ASSISTIDO.getColuna());
+			}
 
-                Filme filme = new Filme(titulo, ano, data, idioma, diretor, diretores, genero, generos, runtime);
-                filmes.add(filme);
-            }
-        } catch (IOException e) {
-            logger.error("Arquivo não encontrado!");
-        }
+			int ano = Integer.parseInt(record.get(Coluna.ANO_LANCAMENTO.getColuna()));
+			Idioma idioma = new Idioma(record.get(Coluna.IDIOMA.getColuna()));
 
-        return filmes;
-    }
+			if (record.get(Coluna.DIRETOR.getColuna()).contains(",")) {
+				diretores = mapearDiretores(record.get(Coluna.DIRETOR.getColuna()));
+			} else {
+				diretor = new Diretor(record.get(Coluna.DIRETOR.getColuna()));
+			}
 
-    public static List<String> getAbreviacoes() {
-        List<String> abreviacoes = new ArrayList<>();
-        String destino = TipoDeConsulta.ABREVIACOES.getDestino();
+			int runtime = Integer.parseInt(record.get(Coluna.DURACAO.getColuna()).trim());
 
-        try {
-        	Reader in = new FileReader(destino);
+			if (record.get(Coluna.GENERO.getColuna()).contains(",")) {
+				generos = mapearGeneros(record.get(Coluna.GENERO.getColuna()));
+			} else {
+				genero = new Genero(record.get(Coluna.GENERO.getColuna()));
+			}
 
-            Iterable<CSVRecord> records = CSVFormat.DEFAULT
-            		.withFirstRecordAsHeader()
-            		.parse(in);
+			Filme filme = new Filme(titulo, ano, data, idioma, diretor, diretores, genero, generos, runtime);
+			filmes.add(filme);
+		}
 
-            for (CSVRecord record : records) {
-            	abreviacoes.add(record.get("idioma") + "," + record.get("abreviacao"));
-            }
-        } catch (IOException e) {
-            logger.error("Arquivo não encontrado!");
-        }
+		return filmes;
+	}
 
-        return abreviacoes;
-    }
+	public static List<String> getAbreviacoes() {
+		List<String> abreviacoes = new ArrayList<>();
+		String destino = TipoDeConsulta.ABREVIACOES.getDestino();
 
-    public static List<String> getDadosDaColuna(Coluna coluna) {
-        List<String> dadosDaColuna = new ArrayList<>();
-        String destino = TipoDeConsulta.VISTOS.getDestino();
-        String nomeDaColuna = coluna.getColuna();
+		Iterable<CSVRecord> records = lerArquivoCsv(destino);
 
-        try {
-        	Reader in = new FileReader(destino);
+		for (CSVRecord record : records) {
+			abreviacoes.add(record.get("idioma") + "," + record.get("abreviacao"));
+		}
 
-            Iterable<CSVRecord> records = CSVFormat.DEFAULT
-            		.withFirstRecordAsHeader()
-            		.parse(in);
+		return abreviacoes;
+	}
 
-            for (CSVRecord record : records) {
-            	dadosDaColuna.add(record.get(nomeDaColuna));
-            }
-        } catch (IOException e) {
-            logger.error("Arquivo não encontrado!");
-        }
+	public static List<String> getDadosDaColuna(Coluna coluna) {
+		List<String> dadosDaColuna = new ArrayList<>();
+		String destino = TipoDeConsulta.VISTOS.getDestino();
+		String nomeDaColuna = coluna.getColuna();
 
-        return dadosDaColuna;
-    }
-    
+		Iterable<CSVRecord> records = lerArquivoCsv(destino);
+
+		for (CSVRecord record : records) {
+			dadosDaColuna.add(record.get(nomeDaColuna));
+		}
+
+		return dadosDaColuna;
+	}
+
+	private static Iterable<CSVRecord> lerArquivoCsv(String destino) {
+		try {
+			Reader in = new FileReader(destino);
+			Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+			return records;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
